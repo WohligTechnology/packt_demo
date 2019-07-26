92fdda2037fd63fbf181ca63c3c34148dea569b7
deshmukhsanket <sanketdeshmukh7206@gmail.com>
2019-07-26 15:59:48 +0530

    Done

--- js/starwarsservice.js
+++ js/starwarsservice.js
@@ -1,36 +1,47 @@
 var starwarsservicemod = angular.module("starwarsservicemod", []);
 starwarsservicemod.factory("StarwarService", function($http) {
-  return {
-    callStarWars: function(param, event, itemsPerPage, searchText, callback) {
-      var returnObj = {};
-      var url = "";
+  function callStarWarsPagination(
+    param,
+    event,
+    itemsPerPage,
+    searchText,
+    callback
+  ) {
+    var returnObj = {};
+    var url = "";
+    returnObj.page = 1;
+    if (event == "pageChange") {
+      returnObj.page = param;
+      url = "?page=" + returnObj.page;
+      if (searchText) {
+        url = url + "&search=" + searchText;
+      }
+    }
+    if (event == "search") {
       returnObj.page = 1;
-      if (event == "pageChange") {
-        returnObj.page = param;
-        url = "?page=" + returnObj.page;
-        if (searchText) {
-          url = url + "&search=" + searchText;
-        }
+      url = "?search=" + param;
+    }
+    $http({
+      method: "GET",
+      url: "https://swapi.co/api/people/" + url
+    }).then(
+      function successCallback(response) {
+        returnObj.totalStarWarsCharacters = response.data.count;
+        returnObj.starWarsCharacters = response.data.results;
+        returnObj.starWarsCharactersPageIndex =
+          (returnObj.page - 1) * itemsPerPage;
+        callback(null, returnObj);
+      },
+      function errorCallback(response) {
+        callback("Error", response);
       }
-      if (event == "search") {
-        returnObj.page = 1;
-        url = "?search=" + param;
-      }
-      $http({
-        method: "GET",
-        url: "https://swapi.co/api/people/" + url
-      }).then(
-        function successCallback(response) {
-          returnObj.totalStarWarsCharacters = response.data.count;
-          returnObj.starWarsCharacters = response.data.results;
-          returnObj.starWarsCharactersPageIndex =
-            (returnObj.page - 1) * itemsPerPage;
-          callback(null, returnObj);
-        },
-        function errorCallback(response) {
-          callback("Error", response);
-        }
-      );
+    );
+  }
+  var returnObj = {
+    callStarWars: callStarWarsPagination,
+    getAllStarWarsCharacter: function() {
+      
     }
   };
+  return returnObj;
 });
