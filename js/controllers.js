844230d7b6a408effbb375acea85ed773ae0d320
Chintan Shah <chintan@wohlig.com>
2019-07-26 12:24:50 +0530

    changes for the frontend

--- js/controllers.js
+++ js/controllers.js
@@ -1,20 +1,36 @@
-angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider', 'ksSwiper'])
+angular
+  .module("phonecatControllers", [
+    "templateservicemod",
+    "navigationservice",
+    "ui.bootstrap",
+    "ngSanitize"
+  ])
 
-.controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+  .controller("HomeCtrl", function(
+    $scope,
+    TemplateService,
+    NavigationService,
+    $timeout
+  ) {
     $scope.template = TemplateService.changecontent("home"); //Use same name of .html file
     $scope.menutitle = NavigationService.makeactive("Home"); //This is the Title of the Website
     TemplateService.title = $scope.menutitle;
     $scope.navigation = NavigationService.getnav();
 
     $scope.mySlides = [
-        'http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg',
-        'http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg',
-        'http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg',
-        'http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg'
+      "http://flexslider.woothemes.com/images/kitchen_adventurer_cheesecake_brownie.jpg",
+      "http://flexslider.woothemes.com/images/kitchen_adventurer_lemon.jpg",
+      "http://flexslider.woothemes.com/images/kitchen_adventurer_donut.jpg",
+      "http://flexslider.woothemes.com/images/kitchen_adventurer_caramel.jpg"
     ];
-})
+  })
 
-.controller('FormCtrl', function($scope, TemplateService, NavigationService, $timeout) {
+  .controller("FormCtrl", function(
+    $scope,
+    TemplateService,
+    NavigationService,
+    $timeout
+  ) {
     $scope.template = TemplateService.changecontent("form"); //Use same name of .html file
     $scope.menutitle = NavigationService.makeactive("Form"); //This is the Title of the Website
     TemplateService.title = $scope.menutitle;
@@ -23,40 +39,46 @@ angular.module('phonecatControllers', ['templateservicemod', 'navigationservice'
     $scope.formSubmitted = false;
 
     $scope.submitForm = function(data) {
-        console.log(data);
-        $scope.formSubmitted = true;
-    }
-})
+      console.log(data);
+      $scope.formSubmitted = true;
+    };
+  })
 
-.controller('headerctrl', function($scope, TemplateService) {
+  .controller("headerctrl", function($scope, TemplateService) {
     $scope.template = TemplateService;
-    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-        $(window).scrollTop(0);
+    $scope.$on("$stateChangeSuccess", function(
+      event,
+      toState,
+      toParams,
+      fromState,
+      fromParams
+    ) {
+      $(window).scrollTop(0);
     });
     $.fancybox.close(true);
-})
+  })
 
-.controller('languageCtrl', function($scope, TemplateService, $translate, $rootScope) {
-
+  .controller("languageCtrl", function(
+    $scope,
+    TemplateService,
+    $translate,
+    $rootScope
+  ) {
     $scope.changeLanguage = function() {
-        console.log("Language CLicked");
+      console.log("Language CLicked");
 
-        if (!$.jStorage.get("language")) {
-            $translate.use("hi");
-            $.jStorage.set("language", "hi");
+      if (!$.jStorage.get("language")) {
+        $translate.use("hi");
+        $.jStorage.set("language", "hi");
+      } else {
+        if ($.jStorage.get("language") == "en") {
+          $translate.use("hi");
+          $.jStorage.set("language", "hi");
         } else {
-            if ($.jStorage.get("language") == "en") {
-                $translate.use("hi");
-                $.jStorage.set("language", "hi");
-            } else {
-                $translate.use("en");
-                $.jStorage.set("language", "en");
-            }
+          $translate.use("en");
+          $.jStorage.set("language", "en");
         }
-        //  $rootScope.$apply();
+      }
+      //  $rootScope.$apply();
     };
-
-
-})
-
-;
+  });
--- js/navigation.js
+++ js/navigation.js
@@ -1,43 +1,42 @@
 var adminURL = "";
-if (isproduction) {
-    adminURL = "http://www.wohlig.co.in/demo/index.php";
-} else {
-    adminURL = "http://localhost/demo/index.php";
-}
+var navigationservice = angular
+  .module("navigationservice", [])
 
-var navigationservice = angular.module('navigationservice', [])
-
-.factory('NavigationService', function() {
-    var navigation = [{
+  .factory("NavigationService", function() {
+    var navigation = [
+      {
         name: "Home",
         classis: "active",
         anchor: "home",
-        subnav: [{
+        subnav: [
+          {
             name: "Subnav1",
             classis: "active",
             anchor: "home"
-        }]
-    }, {
+          }
+        ]
+      },
+      {
         name: "Form",
         classis: "active",
         anchor: "form",
         subnav: []
-    }];
+      }
+    ];
 
     return {
-        getnav: function() {
-            return navigation;
-        },
-        makeactive: function(menuname) {
-            for (var i = 0; i < navigation.length; i++) {
-                if (navigation[i].name == menuname) {
-                    navigation[i].classis = "active";
-                } else {
-                    navigation[i].classis = "";
-                }
-            }
-            return menuname;
-        },
-
+      getnav: function() {
+        return navigation;
+      },
+      makeactive: function(menuname) {
+        for (var i = 0; i < navigation.length; i++) {
+          if (navigation[i].name == menuname) {
+            navigation[i].classis = "active";
+          } else {
+            navigation[i].classis = "";
+          }
+        }
+        return menuname;
+      }
     };
-});
+  });
