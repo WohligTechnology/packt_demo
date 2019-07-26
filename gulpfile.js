4176809255746eddf159ef28c312ce6363e15063
Chintan Shah <chintan@wohlig.com>
2019-07-26 18:41:18 +0530

    changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var browserSync = require("browser-sync").create();
 sass.compiler = require("node-sass");
-
+var concat = require("gulp-concat");
 gulp.task("sass", function() {
   return gulp
     .src("./sass/**/*.scss")
@@ -20,4 +20,26 @@ gulp.task("default", function() {
       baseDir: "./"
     }
   });
+});
+
+var jsArr = [
+  "node_modules/jquery/dist/jquery.js",
+  "node_modules/bootstrap/dist/js/bootstrap.min.js",
+  "node_modules/angular/angular.js",
+  "node_modules/angular-ui-router/release/angular-ui-router.min.js",
+  "node_modules/lodash/lodash.js",
+  "node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js",
+  "node_modules/async/dist/async.min.js",
+  "js/app.js",
+  "js/controllers.js",
+  "js/navigation.js",
+  "js/templateservice.js",
+  "js/starwarsservice.js"
+];
+
+gulp.task("scripts", function() {
+  return gulp
+    .src(jsArr)
+    .pipe(concat("concat.js"))
+    .pipe(gulp.dest("./js"));
 });
